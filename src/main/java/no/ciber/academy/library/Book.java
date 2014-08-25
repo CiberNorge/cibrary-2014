c60e83aef38c5359d96d12a11010318c247c4e7c
Aleks Gisvold <aleksander.gisvold@ciber.com>
2014-08-25 12:43:07 +0200

    added fields for author, releasedate and weblink for a book

--- src/main/java/no/ciber/academy/library/Book.java
+++ src/main/java/no/ciber/academy/library/Book.java
@@ -1,4 +1,5 @@
 package no.ciber.academy.library;
+import org.joda.time.LocalDate;
 
 /**
  * Created by alegis on 25/08/14.
@@ -6,9 +7,43 @@ package no.ciber.academy.library;
 public class Book {
 
     private long Id;
+
+
     private String bookName;
+    private String author;
+    private LocalDate releaseDate;
+    private String webLink;
     private long ISBN;
     //TODO: Field for which office owns the book?
+
+    public Book(){
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
 
     public long getISBN() {
         return ISBN;
