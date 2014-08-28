80e8fa35598c77a25ea427de6925303398566bd0
Jorn Anders Svendsen <jorn.anders.svendsen@gmail.com>
2014-08-28 13:22:32 +0200

    add query as path variable

--- src/main/java/no/ciber/academy/web/controller/BookController.java
+++ src/main/java/no/ciber/academy/web/controller/BookController.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -46,10 +48,10 @@ public class BookController {
         return "books/allBooks";
     }
 
-    @RequestMapping(value = "/search")
-    public String search() {
-        List<Book> jorn = searchService.search("jorn");
-        return null;
+    @RequestMapping(value = "/search/{query}")
+    public String search(Model model, @PathVariable String query) {
+        model.addAttribute("books", searchService.search(query));
+        return "books/allBooks";
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
