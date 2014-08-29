ca1a663ca29a9acce7f6d716ddce6843279b8db9
CharlotteSjothun <charlotte.sjothun@gmail.com>
2014-08-29 11:05:40 +0200

    Fikset en feil i repositoriene

--- src/main/java/no/ciber/academy/repository/BookRepository.java
+++ src/main/java/no/ciber/academy/repository/BookRepository.java
@@ -3,11 +3,13 @@ package no.ciber.academy.repository;
 import no.ciber.academy.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by chasjo on 25/08/14.
  */
 @Repository
+@Transactional
 public interface BookRepository extends JpaRepository<Book, String>{
 
 
